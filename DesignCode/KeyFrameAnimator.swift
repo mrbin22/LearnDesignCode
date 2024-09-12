f751db085534401fbc43fa63d6e0c936bfdeb1c2
mrbin22 <23cm0122@jec.ac.jp>
2024-09-12 22:34:39 +0900

    updated

---
+++ DesignCode/KeyFrameAnimator.swift
@@ -0,0 +1,18 @@
+//
+//  KeyFrameAnimator.swift
+//  DesignCode
+//
+//  Created by cmStudent on 2024/09/12.
+//
+
+import SwiftUI
+
+struct KeyFrameAnimator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    KeyFrameAnimator()
+}
